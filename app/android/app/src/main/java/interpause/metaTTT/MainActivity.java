26c86db4488d06f1c2c66093aaa87ad983b2419f
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-08-08 16:35:15 +0800

    capacitor workflow + android platform

---
+++ app/android/app/src/main/java/interpause/metaTTT/MainActivity.java
@@ -0,0 +1,5 @@
+package interpause.metaTTT;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'interpause.metaTTT',
+  appName: 'MetaTTT',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
